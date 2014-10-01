5091880b6bb282c87baadcc750877040f821a26d
Marc Garreau <marcdgarreau@gmail.com>
2014-10-01 16:15:27 -0600

    pain cycle - linkedin api route

--- app/controllers/omniauth_callbacks_controller.rb
+++ app/controllers/omniauth_callbacks_controller.rb
@@ -1,49 +1,37 @@
 class OmniauthCallbacksController < Devise::OmniauthCallbacksController
 
-  STATE        = SecureRandom.hex(15)
-  REDIRECT_URI = 'http://localhost:3000/users/auth/linkedin/callback'
+  STATE           = SecureRandom.hex(15)
+  REDIRECT_URI    = 'http://localhost:3000/users/auth/linkedin/callback'
+  REDIRECT_URI_II = 'http://localhost:3000/my_profile'
 
   def all
     user = User.from_omniauth(request.env["omniauth.auth"])
-
-    client = LinkedIn::Client.new(ENV["LI_KEY"], ENV["LI_SECRET"])
+    client = OAuth2::Client.new(
+      ENV["LI_KEY"],
+      ENV["LI_SECRET"],
+      :authorize_url => "/uas/oauth2/authorization?response_type=code",
+      :token_url     => "/uas/oauth2/accessToken",
+      :site          => "https://www.linkedin.com"
+    )
+    code = params[:code]
     binding.pry
-
-    request_token = client.request_token({}, :scope => "r_fullprofile r_emailaddress r_network")
-    rtoken = request_token.token
-    rsecret = request_token.secret
-    redirect_to request_token.authorize_url
-
+    token = client.auth_code.get_token(
+      code,
+      :state        => STATE,
+      :redirect_uri => REDIRECT_URI
+    )
     binding.pry
-    client.authorize_from_request(rtoken, rsecret, pin)
-    # response =  HTTParty.get("http://api.linkedin.com/v1/people/~/suggestions/job-suggestions")
-#    client = OAuth2::Client.new(
-#      ENV["LI_KEY"],
-#      ENV["LI_SECRET"],
-#      :authorize_url => "/uas/oauth2/authorization?response_type=code",
-#      :token_url     => "/uas/oauth2/accessToken",
-#      :site          => "https://www.linkedin.com"
-#    )
-#    u_token = request.env["omniauth.auth"]["credentials"].token
-#
-#    redirect_to client.auth_code.authorize_url(
-#      :scope => 'r_fullprofile r_emailaddress r_network'
-#    )
+#    token = client.auth_code.get_token = (params[:code], :redirect_uri => 'http://localhost:3000/users/auth/linkedin/callback')
+    access_token = OAuth2::AccessToken.new(client, token.token, {
+      :mode => :header,
+      :header_format => 'Bearer %s'
+    })
+
+    response = access_token.get("http://api.linkedin.com/v1/people/~/suggestions/job-suggestions")
+
 #  end
-#
+
 #  def my_profile
-#    binding.pry
-#    token = client.auth_code.get_token(
-#      # url.code,
-#      :state        => STATE,
-#      :redirect_uri => REDIRECT_URI
-#    )
-#    access_token = OAuth2::AccessToken.new(client, u_token, {
-#      :mode   => :header,
-#      :header => 'Bearer %s'
-#    })
-#
-#    response = access_token.get("http://api.linkedin.com/v1/people/~/suggestions/job-suggestions")
 
     binding.pry
     if user.persisted?
