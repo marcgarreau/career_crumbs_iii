cf085d64cc83df2bd72530697781bec8b055bf9e
Marc Garreau <marcdgarreau@gmail.com>
2014-10-01 19:08:32 -0600

    holy hell, it might actually work

--- app/controllers/omniauth_callbacks_controller.rb
+++ app/controllers/omniauth_callbacks_controller.rb
@@ -1,47 +1,60 @@
 class OmniauthCallbacksController < Devise::OmniauthCallbacksController
 
-  STATE           = SecureRandom.hex(15)
-  REDIRECT_URI    = 'http://localhost:3000/users/auth/linkedin/callback'
-  REDIRECT_URI_II = 'http://localhost:3000/my_profile'
+  STATE            = SecureRandom.hex(15)
+  REDIRECT_URI_II  = 'http://localhost:3000/users/auth/linkedin/callback'
+  REDIRECT_URI     = 'http://localhost:3000/accept'
 
-  def all
-    user = User.from_omniauth(request.env["omniauth.auth"])
+  def client
     client = OAuth2::Client.new(
       ENV["LI_KEY"],
       ENV["LI_SECRET"],
       :authorize_url => "/uas/oauth2/authorization?response_type=code",
       :token_url     => "/uas/oauth2/accessToken",
-      :site          => "https://www.linkedin.com"
+      :site          => "https://www.linkedin.com",
+      :ssl           => { :verify => OpenSSL::SSL::VERIFY_NONE }
     )
-    code = params[:code]
-    binding.pry
-    token = client.auth_code.get_token(
-      code,
-      :state        => STATE,
+  end
+
+  def all
+    @user = User.from_omniauth(request.env["omniauth.auth"])
+    authorize
+  end
+
+  def index
+    authorize
+  end
+
+  def authorize
+    redirect_to client.auth_code.authorize_url(
+      :scope => "r_fullprofile rw_nus r_emailaddress r_network",
+      :state => STATE,
       :redirect_uri => REDIRECT_URI
     )
-    binding.pry
-#    token = client.auth_code.get_token = (params[:code], :redirect_uri => 'http://localhost:3000/users/auth/linkedin/callback')
-    access_token = OAuth2::AccessToken.new(client, token.token, {
-      :mode => :header,
-      :header_format => 'Bearer %s'
-    })
+  end
 
-    response = access_token.get("http://api.linkedin.com/v1/people/~/suggestions/job-suggestions")
-
-#  end
-
-#  def my_profile
-
-    binding.pry
-    if user.persisted?
-      flash.notice = "Signed in!"
-      sign_in_and_redirect user
+  def accept
+    code  = params[:code]
+    state = params[:state]
+    if state != STATE
+      raise "state has changed. that's fishy."
     else
-      session["devise.user_attributes"] = user.attributes
-      flash.notice = "Something went wrong"
-      redirect_to new_user_session_path
+      token = client.auth_code.get_token(code, :redirect_uri => REDIRECT_URI)
+      access_token = OAuth2::AccessToken.new(client, token.token, {
+        :mode => :header,
+        :header_format => 'Bearer %s',
+        :ssl => {:verify => false}
+      })
+      binding.pry
     end
+    response = access_token.get("http://api.linkedin.com/v1/people/~/suggestions/job-suggestions")
+#    if user.persisted?
+#      flash.notice = "Signed in!"
+#      sign_in_and_redirect user
+#    else
+#      session["devise.user_attributes"] = user.attributes
+#      flash.notice = "Something went wrong"
+#      redirect_to new_user_session_path
+#    end
   end
 
 
--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,8 @@ Rails.application.routes.draw do
   root to: 'pages#welcome'
 
   get 'profile', to: 'pages#profile', as: 'profile'
-  get 'my_profile', to: 'omniauth_callbacks', as: 'my_profile'
-
+  devise_scope :user do
+    get 'my_profile', to: 'omniauth_callbacks#my_profile', as: 'my_profile'
+    get 'accept', to: 'omniauth_callbacks#accept', as: 'accept'
+  end
 end
